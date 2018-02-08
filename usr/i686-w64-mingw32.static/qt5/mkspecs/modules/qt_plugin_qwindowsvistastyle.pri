16644a95b33ace574ec92e3159bb7a798e2f2a33
ilyayunkin <ilya.yunkin@ya.ru>
2018-02-08 22:49:37 +0600

    builded qtbase + qtserialport for windows

---
+++ usr/i686-w64-mingw32.static/qt5/mkspecs/modules/qt_plugin_qwindowsvistastyle.pri
@@ -0,0 +1,5 @@
+QT_PLUGIN.qwindowsvistastyle.TYPE = styles
+QT_PLUGIN.qwindowsvistastyle.EXTENDS =
+QT_PLUGIN.qwindowsvistastyle.DEPENDS = s,-private$,_private,g
+QT_PLUGIN.qwindowsvistastyle.CLASS_NAME = QWindowsVistaStylePlugin
+QT_PLUGINS += qwindowsvistastyle
---
+++ usr/lib/gcc/i686-w64-mingw32.static/5.4.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/home/user/mxe/tmp-gcc-i686-w64-mingw32.static/gcc-5.4.0/configure ac_cv_prog_HAVE_DOXYGEN=false --enable-doc=no --enable-gtk-doc=no --enable-gtk-doc-html=no --enable-gtk-doc-pdf=no --docdir=/home/user/mxe/tmp-gcc-i686-w64-mingw32.static/gcc-5.4.0.build_.sink --infodir=/home/user/mxe/tmp-gcc-i686-w64-mingw32.static/gcc-5.4.0.build_.sink --mandir=/home/user/mxe/tmp-gcc-i686-w64-mingw32.static/gcc-5.4.0.build_.sink --with-html-dir=/home/user/mxe/tmp-gcc-i686-w64-mingw32.static/gcc-5.4.0.build_.sink --disable-doxygen --target=i686-w64-mingw32.static --build=i686-pc-linux-gnu --prefix=/home/user/mxe/usr --libdir=/home/user/mxe/usr/lib --enable-languages=c,c++,objc,fortran --enable-version-specific-runtime-libs --with-gcc --with-gnu-ld --with-gnu-as --disable-nls --disable-shared --disable-multilib --without-x --disable-win32-registry --enable-threads=win32 --enable-sjlj-exceptions --enable-libgomp --with-gmp=/home/user/mxe/usr/i686-pc-linux-gnu --with-isl=/home/user/mxe/usr/i686-pc-linux-gnu --with-mpc=/home/user/mxe/usr/i686-pc-linux-gnu --with-mpfr=/home/user/mxe/usr/i686-pc-linux-gnu --with-as=/home/user/mxe/usr/bin/i686-w64-mingw32.static-as --with-ld=/home/user/mxe/usr/bin/i686-w64-mingw32.static-ld --with-nm=/home/user/mxe/usr/bin/i686-w64-mingw32.static-nm";
+static const char thread_model[] = "win32";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "generic" }, { "arch_32", "pentiumpro" }, { "arch_64", "x86-64" } };
